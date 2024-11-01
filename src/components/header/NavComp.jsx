1994741b10387360274869b50debaeac39c7765d
AliAhmed205 <118130116+AliAhmed205@users.noreply.github.com>
2024-11-02 00:00:14 +0100

    Responsive mediaquery added. Navbar button not functional yet.

--- src/components/header/NavComp.jsx
+++ src/components/header/NavComp.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const NavComp = () => {
   const [activeMenu, setActiveMenu] = useState(null);
+  const [isMenuActive, setIsMenuActive] = useState(false);
   const menuRef = useRef(null);
 
   const toggleMenu = (menuIndex) => {
@@ -13,99 +14,137 @@ const NavComp = () => {
     }
   };
 
+  const togglePhoneMenu = () => {
+    const newState = !isMenuActive;
+    console.log('Toggle menu active state:', newState); // Controleer de nieuwe status
+    setIsMenuActive(newState);
+  };
+  
+  // In de render sectie
+  console.log('isMenuActive:', isMenuActive); // Log de huidige status in de render
+  
+
   useEffect(() => {
     const clickOutside = (event) => {
       if (menuRef.current && !menuRef.current.contains(event.target)) {
         setActiveMenu(null);
+        setIsMenuActive(false); 
       }
     };
 
     document.addEventListener("click", clickOutside);
-    
     return () => {
       document.removeEventListener("click", clickOutside);
     };
   }, []);
 
   return (
-    <nav className="navBar">
-      <section className="a_flex padding" ref={menuRef}>
-        <a href="/">
-          <i className="fa-solid fa-paw"></i> PawPal
-        </a>
+    <>
+      <nav className="navBar">
+        <section className="a_flex padding" ref={menuRef}>
+          <a href="/">
+            <i className="fa-solid fa-paw"></i> PawPal
+          </a>
           <h2>Zes voeten zijn beter dan twee</h2>
-        <ul className="menu-bar a_flex gap-1">
-          <li>
-            <button
-              className="uppercase"
-              onClick={() => toggleMenu(1)}
-              aria-haspopup="true"
-              aria-expanded={activeMenu === 1}
-            >
-              zoek een dier
-              <i className={`fa-solid fa-arrow-down ${activeMenu === 1 ? "turnActive" : ""}`}></i>
-            </button>
-            <ul className={`sub-menu ${activeMenu === 1 ? "active" : ""}`}>
-              <li>
-                <Link className="uppercase" style={{ '--x': '.0s' }} to="/katten">katten</Link>
-              </li>
-              <li>
-                <Link className="uppercase" style={{ '--x': '.1s' }} to="/honden">honden</Link>
-              </li>
-              <li>
-                <Link className="uppercase" style={{ '--x': '.2s' }} to="/vogels">vogels</Link>
-              </li>
-            </ul>
-          </li>
+          <ul className="menu-bar a_flex gap-1">
+            <li>
+              <button
+                className="uppercase"
+                onClick={() => toggleMenu(1)}
+                aria-haspopup="true"
+                aria-expanded={activeMenu === 1}
+              >
+                zoek een dier
+                <i
+                  className={`fa-solid fa-arrow-down ${
+                    activeMenu === 1 ? "turnActive" : ""
+                  }`}
+                ></i>
+              </button>
+              <ul className={`sub-menu ${activeMenu === 1 ? "active" : ""}`}>
+                <li>
+                  <Link className="uppercase" style={{ "--x": ".0s" }} to="/katten">
+                    katten
+                  </Link>
+                </li>
+                <li>
+                  <Link className="uppercase" style={{ "--x": ".1s" }} to="/honden">
+                    honden
+                  </Link>
+                </li>
+                <li>
+                  <Link className="uppercase" style={{ "--x": ".2s" }} to="/vogels">
+                    vogels
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <button
+                className="uppercase"
+                onClick={() => toggleMenu(2)}
+                aria-haspopup="true"
+                aria-expanded={activeMenu === 2}
+              >
+                over ons
+                <i
+                  className={`fa-solid fa-arrow-down ${
+                    activeMenu === 2 ? "turnActive" : ""
+                  }`}
+                ></i>
+              </button>
+            </li>
+            <li>
+              <button
+                className="uppercase"
+                onClick={() => toggleMenu(3)}
+                aria-haspopup="true"
+                aria-expanded={activeMenu === 3}
+              >
+                tips
+                <i
+                  className={`fa-solid fa-arrow-down ${
+                    activeMenu === 3 ? "turnActive" : ""
+                  }`}
+                ></i>
+              </button>
+            </li>
+          </ul>
+        </section>
+      </nav>
 
-          <li>
-            <button
-              className="uppercase"
-              onClick={() => toggleMenu(2)}
-              aria-haspopup="true"
-              aria-expanded={activeMenu === 2}
-            >
-              over ons
-              <i className={`fa-solid fa-arrow-down ${activeMenu === 2 ? "turnActive" : "" }`}></i>
+      <nav className="navBarPhone c_flex">
+        <a href="/">
+          <i className="fa fa-solid fa-paw"></i> Pawpal
+        </a>
+        <button
+          type="button"
+          className="menuButton"
+          onClick={togglePhoneMenu}
+        >
+          <i className="fa fa-solid fa-bars"></i>
+        </button>
+        <ul className={`menu-bar ${isMenuActive ? "reveal" : ""}`}> 
+          <div className="close-container c_flex">
+            <h2>
+              <i className="fa fa-solid fa-paw"></i> Menu
+            </h2>
+            <button onClick={togglePhoneMenu}> 
+              <i className="fa fa-solid fa-x"></i>
             </button>
-            <ul className={`sub-menu ${activeMenu === 2 ? "active" : ""}`}>
-              <li>
-                <Link className="uppercase" style={{ '--x': '0s' }} to="/katten">katten</Link>
-              </li>
-              <li>
-                <Link className="uppercase" style={{ '--x': '.1s' }} to="/honden">honden</Link>
-              </li>
-              <li>
-                <Link className="uppercase" style={{ '--x': '.2s' }} to="/vogels">vogels</Link>
-              </li>
-            </ul>
+          </div>
+          <li>
+            <Link to="/katten">Katten</Link>
           </li>
-
           <li>
-            <button
-              className="uppercase"
-              onClick={() => toggleMenu(3)}
-              aria-haspopup="true"
-              aria-expanded={activeMenu === 3}
-            >
-              tips
-              <i className={`fa-solid fa-arrow-down ${activeMenu === 3 ? "turnActive" : ""}`}></i>
-            </button>
-            <ul className={`sub-menu ${activeMenu === 3 ? "active" : ""}`}>
-              <li>
-                <Link className="uppercase" style={{ '--x': '0s' }} to="/katten">katten</Link>
-              </li>
-              <li>
-                <Link className="uppercase" style={{ '--x': '.1s' }} to="/honden">honden</Link>
-              </li>
-              <li>
-                <Link className="uppercase" style={{ '--x': '.2s' }} to="/vogels">vogels</Link>
-              </li>
-            </ul>
+            <Link to="/honden">Honden</Link>
+          </li>
+          <li>
+            <Link to="/vogels">Vogels</Link>
           </li>
         </ul>
-      </section>
-    </nav>
+      </nav>
+    </>
   );
 };
 
--- src/components/main/AnimalDetail.jsx
+++ src/components/main/AnimalDetail.jsx
@@ -1,27 +1,108 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
 import animalData from "../../animal.json";
-import "../main/Detail.css"
+import "../main/Detail.css";
 const AnimalDetail = () => {
-  const { id } = useParams(); 
-  const animal = animalData.find((animal) => animal.id === parseInt(id)); 
+  const { id } = useParams();
+  const animal = animalData.find((animal) => animal.id === parseInt(id));
 
   return (
-    <section className="animal-detail">
-      <Link to="/adopt"> <i className="fa fa-solid fa-arrow-left"></i> </Link>
+    <section className="animal-detail width">
+      <Link to="/adopt">
+        {" "}
+        <i className="fa fa-solid fa-arrow-left"></i>{" "}
+      </Link>
       <section className="detail">
         <div>
-      <img src={animal.photoUrl} alt={animal.name} />
-      </div>
-      <div>
-      <p><Link to="/">Home</Link>/<Link to="/adopt">Adopt</Link>/<Link to={`/adopt/${animal.id}`}>{animal.name}</Link> </p>
-      <h2>{animal.name}</h2>
-      <p><span>Leeftijd</span>{animal.age} jaar</p>
-      <p><span>Soort</span>{animal.breed}</p>
-      <p><span>Geslacht</span>{animal.gender}</p>
-      <p><span>Status</span>{animal.adoptionStatus}</p>
-      <p><span>Over {animal.name}</span> {animal.description}</p>
-      </div>
+          <img src={animal.photoUrl} alt={animal.name} />
+        </div>
+        <div>
+          <p>
+            <Link
+              style={{
+                "--x": ".1s",
+              }}
+              to="/"
+            >
+              Home
+            </Link>
+            /
+            <Link
+              style={{
+                "--x": ".2s",
+              }}
+              to="/adopt"
+            >
+              Adopt
+            </Link>
+            /
+            <Link
+              style={{
+                "--x": ".3s",
+              }}
+              to={`/adopt/${animal.id}`}
+            >
+              {animal.name}
+            </Link>{" "}
+          </p>
+          <h2
+            style={{
+              "--x": ".1s",
+            }}
+          >
+            {animal.name}
+          </h2>
+          <i style={{
+            "--x" : ".2s"
+          }} className={animal.icon}></i>
+          <p
+            style={{
+              "--x": ".3s",
+            }}
+          >
+            <span>Leeftijd</span>
+            {animal.age} jaar
+          </p>
+          <p
+            style={{
+              "--x": ".4s",
+            }}
+          >
+            <span>Soort</span>
+            {animal.breed}
+          </p>
+          <p
+            style={{
+              "--x": ".5s",
+            }}
+          >
+            <span>Geslacht</span>
+            {animal.gender}
+          </p>
+          <p
+            style={{
+              "--x": ".6s",
+            }}
+          >
+            <span>Status</span>
+            {animal.adoptionStatus}
+          </p>
+          <p
+            style={{
+              "--x": ".7s",
+            }}
+          >
+            <span>Over {animal.name}</span> {animal.description}
+          </p>
+          <a
+            href="mailto:info@itsbyali.com"
+            style={{
+              "--x": ".8s",
+            }}
+          >
+            <i className="fa fa-solid fa-envelope"></i> Neem contact op
+          </a>
+        </div>
       </section>
     </section>
   );
